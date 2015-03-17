7ea416b89ccf0932c5735c809a7c92639d05d325
Julien Vulliet <julien@mrvux.com>
2015-03-17 18:03:39 +0100

    Add kinect client listener

---
+++ src/KGP.Core/Network/FrameServer/KinectClientEventArgs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Network.FrameServer
+{
+    /// <summary>
+    /// Event args raised when we have a new kinect client
+    /// </summary>
+    public class KinectClientEventArgs : EventArgs
+    {
+        private readonly TcpClient client;
+        private readonly NetworkStream stream;
+
+        /// <summary>
+        /// Constructs event args
+        /// </summary>
+        /// <param name="client">TCP Client</param>
+        public KinectClientEventArgs(TcpClient client)
+        {
+            this.client = client;
+            this.stream = client.GetStream();
+        }
+
+        /// <summary>
+        /// Tcp Client
+        /// </summary>
+        public TcpClient Client
+        {
+            get { return this.client; }
+        }
+
+        /// <summary>
+        /// Network stream to receive and send data
+        /// </summary>
+        public NetworkStream NetworkStream
+        {
+            get { return this.stream; }
+        }
+    }
+}
---
+++ src/KGP.Core/Network/FrameServer/KinectClientListener.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KGP.Network.FrameServer
+{
+    /// <summary>
+    /// Listener class for kinect clients
+    /// </summary>
+    public class KinectClientListener : IDisposable
+    {
+        private TcpListener listener;
+        private Thread listenThread;
+
+        private bool isListening = false;
+
+        /// <summary>
+        /// Raised when a client connected
+        /// </summary>
+        public event EventHandler<KinectClientEventArgs> ClientConnected;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="port">Server port</param>
+        public KinectClientListener(int port)
+        {
+            this.listener = new TcpListener(IPAddress.Any, port);
+            this.listenThread = new Thread(new ThreadStart(this.ListenForClients));
+            this.listener.Start();
+            this.listenThread.Start();
+            this.isListening = true;
+        }
+
+        private void ListenForClients()
+        {
+            while (this.isListening)
+            {
+                TcpClient client = this.listener.AcceptTcpClient();
+
+                if (client.Connected)
+                {
+                    if (this.ClientConnected != null)
+                    {
+                        this.ClientConnected(this, new KinectClientEventArgs(client));
+                    }
+
+                    this.isListening = false;
+                    this.listener.Stop();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disposes the client listener, by stopping the listen thread if applicable
+        /// </summary>
+        public void Dispose()
+        {
+            try
+            {
+                this.listenThread.Abort();
+            }
+            catch
+            {
+
+            }
+            this.listener.Stop();
+        }
+    }
+}
