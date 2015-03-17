695efce115f210038bc49eb4bb9fbb4e24f7ef8d
Julien Vulliet <julien@mrvux.com>
2015-03-17 13:58:18 +0100

    Body adapter constructor

--- src/KGP.Core/KinectBody.cs
+++ src/KGP.Core/KinectBody.cs
@@ -8,36 +8,43 @@ using System.Threading.Tasks;
 namespace KGP
 {
     /// <summary>
-    /// Adapter for kinect body, which allows us to get body information from several sources (kinect or network)
+    /// Adapter for kinect body, which allows us to get body information from several sources (kinect or network).
+    /// Class is immutable, so can be accessed fromseveral threads.
+    /// <remarks>Activities, Appearance, Engaged and Expressions are not part of this class, since they are now in Face API.</remarks>
     /// </summary>
     public class KinectBody
     {
-        private readonly IReadOnlyDictionary<Activity, DetectionResult> activities;
-        private readonly IReadOnlyDictionary<Appearance, DetectionResult> appearance;
         private readonly FrameEdges clippedEdges;
-        private readonly DetectionResult engaged;
-        private readonly IReadOnlyDictionary<Expression, DetectionResult> expressions;
         private readonly TrackingConfidence handLeftConfidence;
         private readonly HandState handLeftState;
         private readonly TrackingConfidence handRightConfidence;
         private readonly HandState handRightState;
         private readonly bool isRestricted;
         private readonly bool isTracked;
-        private readonly int jointCount;
         private readonly IReadOnlyDictionary<JointType, JointOrientation> jointOrientations;
         private readonly IReadOnlyDictionary<JointType, Joint> joints;
         private readonly PointF lean;
         private readonly TrackingState leanTrackingState;
         private readonly ulong trackingId;
 
-        public IReadOnlyDictionary<Activity, DetectionResult> Activities
+        /// <summary>
+        /// Constructs a bdy adapter from a kinect sdk body
+        /// </summary>
+        /// <param name="body">Body from Kinect SDK</param>
+        public KinectBody(Microsoft.Kinect.Body body)
         {
-            get { return this.activities; }
-        }
-
-        public IReadOnlyDictionary<Appearance, DetectionResult> Appearance
-        {
-            get { return this.appearance; }
+            this.clippedEdges = body.ClippedEdges;
+            this.handLeftConfidence = body.HandLeftConfidence;
+            this.handLeftState = body.HandLeftState;
+            this.handRightConfidence = body.HandRightConfidence;
+            this.handRightState = body.HandRightState;
+            this.isRestricted = body.IsRestricted;
+            this.isTracked = body.IsTracked;
+            this.jointOrientations = body.JointOrientations;
+            this.joints = body.Joints;
+            this.lean = body.Lean;
+            this.leanTrackingState = body.LeanTrackingState;
+            this.trackingId = body.TrackingId;
         }
 
         public FrameEdges ClippedEdges
@@ -45,16 +52,6 @@ namespace KGP
             get { return this.clippedEdges; }
         }
 
-        public DetectionResult Engaged
-        {
-            get { return this.engaged; }
-        }
-
-        public IReadOnlyDictionary<Expression, DetectionResult> Expressions
-        {
-            get { return this.expressions; }
-        }
-        
         public TrackingConfidence HandLeftConfidence
         {
             get { return this.handLeftConfidence; }
@@ -83,11 +80,6 @@ namespace KGP
         public bool IsTracked
         {
             get { return this.isTracked; }
-        }
-
-        public int JointCount
-        {
-            get { return this.jointCount; }
         }
 
         public IReadOnlyDictionary<JointType,JointOrientation> JointOrientations
