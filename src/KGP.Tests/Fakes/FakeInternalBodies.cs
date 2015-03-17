60cbefee95de26b936c759d13fe540ccecc5b184
Julien Vulliet <julien@mrvux.com>
2015-03-17 15:25:44 +0100

    Add fake data so we can start testing

---
+++ src/KGP.Tests/Fakes/FakeInternalBodies.cs
@@ -0,0 +1,31 @@
+﻿using KGP.Serialization.Body;
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Tests.Fakes
+{
+    public static class FakeInternalBodies
+    {
+        public static KinectBodyInternal NullOrientationsBody()
+        {
+            return new KinectBodyInternal()
+            {
+                ClippedEdges = FrameEdges.None,
+                HandLeftConfidence = TrackingConfidence.High,
+                HandLeftState = HandState.NotTracked,
+                HandRightConfidence = TrackingConfidence.High,
+                HandRightState = HandState.NotTracked,
+                IsRestricted = false,
+                IsTracked = true,
+                JointOrientations = null,
+                Joints = FakeJoints.ValidRandomJoints(),
+                LeanTrackingState = TrackingState.NotTracked,
+                TrackingId = 0231
+            };
+        }
+    }
+}
---
+++ src/KGP.Tests/Fakes/FakeJoints.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Tests.Fakes
+{
+    public static class FakeJoints
+    {
+        public static Joint[] ValidRandomJoints()
+        {
+            Joint[] result = new Joint[Microsoft.Kinect.Body.JointCount];
+            JointType[] jt = (JointType[])Enum.GetValues(typeof(JointType));
+            for (int i = 0; i < result.Length;i++)
+            {
+                result[i].JointType = jt[i];
+            }
+            return result;
+        }
+    }
+}
