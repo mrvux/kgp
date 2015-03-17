e5ca62720e95b6e278697c4dc5d487d063f1022a
Julien Vulliet <julien@mrvux.com>
2015-03-17 18:57:48 +0100

    Add direct3d project

---
+++ src/KGP.Core/Network/FrameServer/KinectFrameClient.cs
@@ -0,0 +1,114 @@
+﻿using KGP.Serialization.Images;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KGP.Network.FrameServer
+{
+    /// <summary>
+    /// Simple quick and dirty TCP Kinect frame client
+    /// </summary>
+    public class KinectFrameClient
+    {
+        private TcpClient client;
+        private IPEndPoint serverEndPoint;
+
+        private NetworkStream clientStream;
+        private Thread receiverThread;
+
+        private readonly SnappyFrameDecompressor depthDecompressor;
+        private readonly SnappyFrameDecompressor bodyIndexDecompressor;
+
+        /// <summary>
+        /// (Ugly) Event when depth frame is received
+        /// </summary>
+        public event EventHandler<SnappyFrameDecompressor> DepthFrameReceived;
+
+        /// <summary>
+        /// (Ugly) Event when body Index frame is received
+        /// </summary>
+        public event EventHandler<SnappyFrameDecompressor> BodyIndexFrameArrived;
+
+        private readonly byte[] temporaryData = new byte[KinectFrameInformation.DepthFrame.FrameDataSize];
+        private byte[] header = new byte[5];
+
+        private bool isRunning;
+
+        /// <summary>
+        /// Constructs a Kinect frame client
+        /// </summary>
+        /// <param name="ipAddress">Server address</param>
+        /// <param name="port">Server port</param>
+        public KinectFrameClient(IPAddress ipAddress, int port)
+        {
+            this.serverEndPoint = new IPEndPoint(ipAddress, port);
+            this.client = new TcpClient();
+            this.depthDecompressor = new SnappyFrameDecompressor(KinectFrameInformation.DepthFrame);
+            this.bodyIndexDecompressor = new SnappyFrameDecompressor(KinectFrameInformation.BodyIndexFrame);
+        }
+
+        /// <summary>
+        /// Connects to client
+        /// </summary>
+        public void Connect()
+        {
+            this.client.Connect(this.serverEndPoint);
+            this.clientStream = client.GetStream();
+            this.isRunning = true;
+            this.receiverThread = new Thread(new ThreadStart(this.ReceiveThread));
+            this.receiverThread.Start();
+        }
+
+        private unsafe void ReceiveThread()
+        {
+            while (this.isRunning)
+            {
+                if (this.clientStream.DataAvailable)
+                {
+                    int p = this.clientStream.Read(this.header, 0, 5);
+
+                    int packetLength = BitConverter.ToInt32(this.header, 0);
+
+                    bool isDepth = this.header[4] == 0;
+
+                    int totalRead = this.clientStream.Read(this.temporaryData, 0, packetLength);
+
+                    fixed (byte* bptr = &this.temporaryData[0])
+                    {
+                        if (isDepth)
+                        {
+                            this.depthDecompressor.UnCompress(new IntPtr(bptr), packetLength);
+                            if (this.DepthFrameReceived != null)
+                            {
+                                this.DepthFrameReceived(this, this.depthDecompressor);
+                            }
+                        }
+                        else
+                        {
+                            this.bodyIndexDecompressor.UnCompress(new IntPtr(bptr), packetLength);
+                            if (this.BodyIndexFrameArrived != null)
+                            {
+                                this.BodyIndexFrameArrived(this, this.depthDecompressor);
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(5);
+            }
+        }
+
+        /// <summary>
+        /// Stop receiving data
+        /// </summary>
+        public void Stop()
+        {
+            this.isRunning = false;
+        }
+     
+    }
+}
---
+++ src/KGP.Core/Network/FrameServer/KinectFrameServer.cs
@@ -0,0 +1,122 @@
+﻿using KGP.Serialization;
+using KGP.Serialization.Images;
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Network.FrameServer
+{
+    /// <summary>
+    /// TCP Frame server for kinect
+    /// <remarks>At the moment for quick test we only stream depth and body index</remarks>
+    /// </summary>
+    public class KinectFrameServer : IDisposable
+    {
+        private KinectSensor sensor;
+        private readonly KinectClientListener listener;
+
+        private TcpClient activeClient;
+        private NetworkStream networkStream;
+
+        private SnappyFrameCompressor depthCompressor;
+        private SnappyFrameCompressor bodyIndexCompressor;
+
+        private MultiSourceFrameReader multiSourceReader;
+        
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="port">Server port</param>
+        /// <param name="sensor"></param>
+        public KinectFrameServer(int port, KinectSensor sensor)
+        {
+            this.sensor = sensor;
+            this.listener = new KinectClientListener(port);
+            this.listener.ClientConnected += listener_ClientConnected;
+
+            this.depthCompressor = new SnappyFrameCompressor(KinectFrameInformation.DepthFrame);
+            this.bodyIndexCompressor = new SnappyFrameCompressor(KinectFrameInformation.BodyIndexFrame);
+
+            this.multiSourceReader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.BodyIndex | FrameSourceTypes.Depth);
+            this.multiSourceReader.MultiSourceFrameArrived += multiSourceReader_MultiSourceFrameArrived;
+        }
+
+        private void multiSourceReader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            if (this.activeClient != null)
+            {
+                MultiSourceFrame frame = e.FrameReference.AcquireFrame();
+                if (frame != null)
+                {
+                    this.ProcessDepth(frame);
+                    this.ProcessBodyIndex(frame);
+
+                    //Push depth frame
+                    int depthPacketsize = this.depthCompressor.CompressedSize;
+
+                    byte[] bheader = BitConverter.GetBytes(depthPacketsize);
+                    this.networkStream.Write(bheader, 0, 4);
+                    this.networkStream.Write(new byte[] { 0 }, 0, 1);
+
+                    this.networkStream.Write(this.depthCompressor.CompressedFrameData, 0, depthPacketsize);
+
+                    
+
+                    //Push body index frame
+                    int bodyIndexPacketsize = this.bodyIndexCompressor.CompressedSize;
+
+                    bheader = BitConverter.GetBytes(bodyIndexPacketsize);
+                    this.networkStream.Write(bheader, 0, 4);
+                    this.networkStream.Write(new byte[] { 1 }, 0, 1);
+                    this.networkStream.Write(this.bodyIndexCompressor.CompressedFrameData, 0, bodyIndexPacketsize);
+                }
+            }
+        }
+
+        private void ProcessDepth(MultiSourceFrame msf)
+        {
+            var depthFrame = msf.DepthFrameReference.AcquireFrame();
+            if (depthFrame != null)
+            {
+                var buffer = depthFrame.LockImageBuffer();
+                this.depthCompressor.Compress(buffer.UnderlyingBuffer);
+                buffer.Dispose();
+
+                depthFrame.Dispose();
+            }
+        }
+
+        private void ProcessBodyIndex(MultiSourceFrame msf)
+        {
+            var bodyIndexFrame = msf.BodyIndexFrameReference.AcquireFrame();
+            if (bodyIndexFrame != null)
+            {
+                var buffer = bodyIndexFrame.LockImageBuffer();
+                this.bodyIndexCompressor.Compress(buffer.UnderlyingBuffer);
+                buffer.Dispose();
+
+                bodyIndexFrame.Dispose();
+            }
+        }
+
+        private void listener_ClientConnected(object sender, KinectClientEventArgs e)
+        {
+            this.activeClient = e.Client;
+            this.networkStream = e.NetworkStream;
+        }
+
+        /// <summary>
+        /// Disposes the frame server
+        /// </summary>
+        public void Dispose()
+        {
+            this.listener.ClientConnected -= listener_ClientConnected;
+            this.listener.Dispose();
+        }
+    }
+}
