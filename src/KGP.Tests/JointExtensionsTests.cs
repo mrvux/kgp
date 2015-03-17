34644a29a7ff5d87ba7ddc6fbf0db17517b56122
Julien Vulliet <julien@mrvux.com>
2015-03-17 16:00:43 +0100

    Joint extensions tests

---
+++ src/KGP.Tests/JointExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Kinect;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Tests
+{
+    [TestClass]
+    public class JointExtensionsTests
+    {
+        [TestMethod]
+        public void TestAtLeastInferred()
+        {
+            var jnot = new Joint()
+            {
+                JointType = JointType.Head,
+                TrackingState = TrackingState.NotTracked
+            };
+
+            var jinferred = new Joint()
+            {
+                JointType = JointType.Head,
+                TrackingState = TrackingState.Inferred
+            };
+
+            var jtracked = new Joint()
+            {
+                JointType = JointType.Head,
+                TrackingState = TrackingState.Tracked
+            };
+
+            Assert.AreEqual(false, jnot.IsAtLeastInferred());
+            Assert.AreEqual(true, jinferred.IsAtLeastInferred());
+            Assert.AreEqual(true, jtracked.IsAtLeastInferred());
+        }
+    }
+}
