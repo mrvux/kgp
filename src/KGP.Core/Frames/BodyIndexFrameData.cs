fab20c02f13e096c0fdbcfac0f128e1e8e375703
Julien Vulliet <julien@mrvux.com>
2015-03-18 16:25:14 +0100

    Add size in bytes for body index frame data
    Add sensor version for body index provider

--- src/KGP.Core/Frames/BodyIndexFrameData.cs
+++ src/KGP.Core/Frames/BodyIndexFrameData.cs
@@ -13,6 +13,7 @@ namespace KGP.Frames
     public class BodyIndexFrameData : IDisposable
     {
         private IntPtr dataPointer;
+        private int sizeInBytes;
 
         /// <summary>
         /// Data pointer for frame data
@@ -29,11 +30,20 @@ namespace KGP.Frames
         }
 
         /// <summary>
+        /// Frame size, in bytes
+        /// </summary>
+        public int SizeInBytes
+        {
+            get { return this.sizeInBytes; }
+        }
+
+        /// <summary>
         /// Constructor, allocates memory to hold frame data
         /// </summary>
         public BodyIndexFrameData()
         {
-            this.dataPointer = Marshal.AllocHGlobal(Consts.DepthWidth*Consts.DepthHeight);
+            this.sizeInBytes = Consts.DepthWidth * Consts.DepthHeight;
+            this.dataPointer = Marshal.AllocHGlobal(this.sizeInBytes);
         }
 
         /// <summary>
@@ -45,6 +55,7 @@ namespace KGP.Frames
             {
                 Marshal.FreeHGlobal(this.dataPointer);
                 this.dataPointer = IntPtr.Zero;
+                this.sizeInBytes = 0;
             }
         }
     }   
---
+++ src/KGP.Core/Providers/Sensor/KinectSensorBodyIndexFrameProvider.cs
@@ -0,0 +1,63 @@
+﻿using KGP.Frames;
+using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Providers.Sensor
+{
+    /// <summary>
+    /// Kinect sensor version for body index frame provider
+    /// </summary>
+    public class KinectSensorBodyIndexFrameProvider : IBodyIndexFrameProvider, IDisposable
+    {
+        private readonly KinectSensor sensor;
+        private BodyIndexFrameReader reader;
+        private BodyIndexFrameData frameData;
+
+        /// <summary>
+        /// Raised when a new body index frame is received
+        /// </summary>
+        public event EventHandler<BodyIndexFrameDataEventArgs> FrameReceived;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="sensor">Kinect sensor</param>
+        public KinectSensorBodyIndexFrameProvider(KinectSensor sensor)
+        {
+            if (sensor == null)
+                throw new ArgumentNullException("sensor");
+
+            this.sensor = sensor;
+            this.reader = this.sensor.BodyIndexFrameSource.OpenReader();
+            this.reader.FrameArrived += this.FrameArrived;
+            this.frameData = new BodyIndexFrameData();
+        }
+
+        /// <summary>
+        /// Dispose frame data
+        /// </summary>
+        public void Dispose()
+        {
+            this.reader.FrameArrived -= this.FrameArrived;
+            this.reader.Dispose();
+            this.frameData.Dispose();
+        }
+
+        private void FrameArrived(object sender, BodyIndexFrameArrivedEventArgs e)
+        {
+            var frame = e.FrameReference.AcquireFrame();
+            if (frame != null)
+            {
+                frame.CopyFrameDataToIntPtr(this.frameData.DataPointer, (uint)this.frameData.SizeInBytes);
+                if (this.FrameReceived != null)
+                {
+                    this.FrameReceived(this, new BodyIndexFrameDataEventArgs(this.frameData));
+                }
+            }
+        }
+    }
+}
