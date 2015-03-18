b46d09d3edaded541533b6d361cae186d2db85f6
Julien Vulliet <julien@mrvux.com>
2015-03-18 18:10:17 +0100

    Add externals for samples
    Add color texture sample and dynamic rgba texture

--- samples/ColorTextureSample/Program.cs
+++ samples/ColorTextureSample/Program.cs
@@ -12,7 +12,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DepthTextureSample
+namespace ColorTextureSample
 {
     static class Program
     {
@@ -25,23 +25,23 @@ namespace DepthTextureSample
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            RenderForm form = new RenderForm("Kinect depth sample");
+            RenderForm form = new RenderForm("Kinect color sample");
 
             RenderDevice device = new RenderDevice(SharpDX.Direct3D11.DeviceCreationFlags.BgraSupport);
             RenderContext context = new RenderContext(device);
-            DX11SwapChain swapChain = DX11SwapChain.FromHandle(device,form.Handle);
+            DX11SwapChain swapChain = DX11SwapChain.FromHandle(device, form.Handle);
 
             KinectSensor sensor = KinectSensor.GetDefault();
             sensor.Open();
 
-            
+
 
             bool doQuit = false;
             bool doUpload = false;
-            DepthFrameData currentData = null;
-            DynamicDepthTexture depth = new DynamicDepthTexture(device);
-            KinectSensorDepthFrameProvider provider = new KinectSensorDepthFrameProvider(sensor);
-            provider.FrameReceived += (sender, args) => { currentData = args.DepthData; doUpload = true; };
+            ColorRGBAFrameData currentData = null;
+            DynamicColorRGBATexture colorTexture = new DynamicColorRGBATexture(device);
+            KinectSensorColorRGBAFrameProvider provider = new KinectSensorColorRGBAFrameProvider(sensor);
+            provider.FrameReceived += (sender, args) => { currentData = args.FrameData; doUpload = true; };
 
             form.KeyDown += (sender, args) => { if (args.KeyCode == Keys.Escape) { doQuit = true; } };
 
@@ -55,12 +55,12 @@ namespace DepthTextureSample
 
                 if (doUpload)
                 {
-                    depth.Copy(context, currentData);
+                    colorTexture.Copy(context, currentData);
                 }
 
                 context.RenderTargetStack.Push(swapChain);
 
-                device.Primitives.ApplyFullTri(context, depth.NormalizedView);
+                device.Primitives.ApplyFullTri(context, colorTexture.ShaderView);
 
                 device.Primitives.FullScreenTriangle.Draw(context);
                 context.RenderTargetStack.Pop();
@@ -71,7 +71,7 @@ namespace DepthTextureSample
             context.Dispose();
             device.Dispose();
 
-            depth.Dispose();
+            colorTexture.Dispose();
             provider.Dispose();
 
             sensor.Close();
---
+++ src/KGP.Direct3D11/Textures/DynamicColorRGBATexture.cs
@@ -0,0 +1,57 @@
+﻿using KGP.Direct3D11.Descriptors;
+using KGP.Frames;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Direct3D11.Textures
+{
+    /// <summary>
+    /// Dynamic rgba color texture
+    /// </summary>
+    public class DynamicColorRGBATexture : IDisposable
+    {
+        private Texture2D texture;
+        private ShaderResourceView rawView;
+
+        /// <summary>
+        /// Raw shader view
+        /// </summary>
+        public ShaderResourceView ShaderView
+        {
+            get { return this.rawView; }
+        }
+
+        /// <summary>
+        /// Creates a dynamic color texture, allocates GPU resources
+        /// </summary>
+        /// <param name="device">Direct3D Device</param>
+        public DynamicColorRGBATexture(Device device)
+        {
+            this.texture = new Texture2D(device, ColorTextureDescriptors.DynamicRGBAResource);
+            this.rawView = new ShaderResourceView(device,this.texture);
+        }
+
+        /// <summary>
+        /// Copy depth frame to graphics card
+        /// </summary>
+        /// <param name="context">Device context</param>
+        /// <param name="data">Color frame data</param>
+        public void Copy(DeviceContext context, ColorRGBAFrameData data)
+        {
+            this.texture.Upload(context, data.DataPointer, data.SizeInBytes);
+        }
+
+        /// <summary>
+        /// Disposes gpu resources
+        /// </summary>
+        public void Dispose()
+        {
+            this.rawView.Dispose();
+            this.texture.Dispose();
+        }
+    }
+}
