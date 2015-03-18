797731267a8e1d3d405895d78f590ef554eff996
mrvux <julien@mrvux.com>
2015-03-18 21:44:38 +0100

    Tests for buffers

---
+++ tests/KDP.Direct3D11.Tests/Buffers/BodyCameraPositionBufferTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KGP.Direct3D11.Textures;
+using KGP.Frames;
+using KGP.Direct3D11.Buffers;
+
+namespace KDP.Direct3D11.Tests.Textures
+{
+    [TestClass]
+    public class BodyCameraPositionBufferTests : IDisposable
+    {
+        private SharpDX.Direct3D11.Device device;
+
+
+        public BodyCameraPositionBufferTests()
+        {
+            device = new SharpDX.Direct3D11.Device(SharpDX.Direct3D.DriverType.Reference);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            using (BodyCameraPositionBuffer buffer = new BodyCameraPositionBuffer(device))
+            {
+                Assert.AreNotEqual(buffer.ShaderView.NativePointer, IntPtr.Zero);
+            }
+        }
+
+        public void Dispose()
+        {
+            device.Dispose();
+        }
+    }
+}
---
+++ tests/KDP.Direct3D11.Tests/Buffers/BodyColorPositionBufferTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KGP.Direct3D11.Textures;
+using KGP.Frames;
+using KGP.Direct3D11.Buffers;
+
+namespace KDP.Direct3D11.Tests.Textures
+{
+    [TestClass]
+    public class BodyColorPositionBufferTests : IDisposable
+    {
+        private SharpDX.Direct3D11.Device device;
+
+
+        public BodyColorPositionBufferTests()
+        {
+            device = new SharpDX.Direct3D11.Device(SharpDX.Direct3D.DriverType.Reference);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            using (BodyColorPositionBuffer buffer = new BodyColorPositionBuffer(device))
+            {
+                Assert.AreNotEqual(buffer.ShaderView.NativePointer, IntPtr.Zero);
+            }
+        }
+
+        public void Dispose()
+        {
+            device.Dispose();
+        }
+    }
+}
