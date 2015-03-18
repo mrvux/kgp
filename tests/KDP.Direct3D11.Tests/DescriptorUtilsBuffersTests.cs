535542aa9b9864a63b338db7039af6b30430c4fc
Julien Vulliet <julien@mrvux.com>
2015-03-18 13:28:23 +0100

    Add tests for descriptors
    note: most invalid do (and should) fail for now, since validation is not implemented

---
+++ tests/KDP.Direct3D11.Tests/DescriptorUtilsBuffersTests.cs
@@ -0,0 +1,74 @@
+﻿using KGP.Direct3D11.Descriptors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDP.Direct3D11.Tests
+{
+    [TestClass]
+    public class DescriptorUtilsBuffersViewTests
+    {
+        [TestMethod]
+        public void DynamicBufferDesc()
+        {
+            var desc = DescriptorUtils.DynamicStructuredBuffer(1024, 32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidElementDynamicBufferDesc()
+        {
+            var desc = DescriptorUtils.DynamicStructuredBuffer(0, 32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidStrideDynamicBufferDesc()
+        {
+            var desc = DescriptorUtils.DynamicStructuredBuffer(1024, 1);
+        }
+
+        [TestMethod]
+        public void ImmutableBufferDesc()
+        {
+            var desc = DescriptorUtils.ImmutableStructuredBuffer(1024, 32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidElementImmutableBufferDesc()
+        {
+            var desc = DescriptorUtils.ImmutableStructuredBuffer(0, 32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidStrideImmutableBufferDesc()
+        {
+            var desc = DescriptorUtils.ImmutableStructuredBuffer(1024, 1);
+        }
+
+        [TestMethod]
+        public void WriteableBufferDesc()
+        {
+            var desc = DescriptorUtils.WriteableStructuredBuffer(1024, 32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidElementWriteBufferDesc()
+        {
+            var desc = DescriptorUtils.WriteableStructuredBuffer(0, 32);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidStrideWriteBufferDesc()
+        {
+            var desc = DescriptorUtils.WriteableStructuredBuffer(1024, 1);
+        }
+    }
+}
---
+++ tests/KDP.Direct3D11.Tests/DescriptorUtilsBuffersViewTests.cs
@@ -0,0 +1,53 @@
+﻿using KGP.Direct3D11.Descriptors;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDP.Direct3D11.Tests
+{
+    [TestClass]
+    public class DescriptorUtilsBuffersTests
+    {
+        [TestMethod]
+        public void WriteView()
+        {
+            var desc = DescriptorUtils.WriteStructuredBufferView(1024);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidElementWriteView()
+        {
+            var desc = DescriptorUtils.WriteStructuredBufferView(0);
+        }
+
+        [TestMethod]
+        public void AppendView()
+        {
+            var desc = DescriptorUtils.AppendStructuredBufferView(1024);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidElementAppendView()
+        {
+            var desc = DescriptorUtils.AppendStructuredBufferView(0);
+        }
+
+        [TestMethod]
+        public void CounterView()
+        {
+            var desc = DescriptorUtils.CounterStructuredBufferView(1024);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidElementCounterView()
+        {
+            var desc = DescriptorUtils.CounterStructuredBufferView(0);
+        }
+    }
+}
