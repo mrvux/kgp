42ffd7c73f0d238c5d0b380c16567a5595faa249
mrvux <julien@mrvux.com>
2015-03-18 22:20:30 +0100

    add infrared provider

---
+++ src/KGP.Core/Providers/IInfraredFrameProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Providers
+{
+    /// <summary>
+    /// Interface to receive infrared frames
+    /// </summary>
+    public interface IInfraredFrameProvider
+    {
+        /// <summary>
+        /// Raised when infrared frame is received
+        /// </summary>
+        event EventHandler<InfraredFrameDataEventArgs> FrameReceived;
+    }
+}
