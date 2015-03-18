8d9ecc01e3b4cac9381250d60911de7af4d41770
Julien Vulliet <julien@mrvux.com>
2015-03-18 19:23:30 +0100

    Add body frame provider

---
+++ src/KGP.Core/Providers/IBodyFrameProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Providers
+{
+    /// <summary>
+    /// Interface to receive body frames
+    /// </summary>
+    public interface IBodyFrameProvider
+    {
+        /// <summary>
+        /// Raised when body frame is received
+        /// </summary>
+        event EventHandler<KinectBodyFrameDataEventArgs> FrameReceived;
+    }
+}
---
+++ src/KGP.Core/Providers/KinectBodyFrameDataEventArgs.cs
@@ -0,0 +1,36 @@
+﻿using KGP.Frames;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Providers
+{ 
+    /// <summary>
+    ///  Event args wrapper for <see cref="KinectBody"/>
+    /// </summary>
+    public class KinectBodyFrameDataEventArgs : EventArgs
+    {
+        private readonly KinectBody[] args;
+
+        /// <summary>
+        /// Receive frame data
+        /// </summary>
+        public KinectBody[] FrameData
+        {
+            get { return this.args; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public KinectBodyFrameDataEventArgs(KinectBody[] args)
+        {
+            if (args == null)
+                throw new ArgumentNullException("args");
+            this.args = args;
+        }
+    }   
+        
+}
