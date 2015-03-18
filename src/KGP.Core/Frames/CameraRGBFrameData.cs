24496d98e75e49afbeb84c8c17fd6d90099971c9
Julien Vulliet <julien@mrvux.com>
2015-03-18 14:05:11 +0100

    Add camera rgb frame data

---
+++ src/KGP.Core/Frames/CameraRGBFrameData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Frames
+{
+    /// <summary>
+    /// Camera space frame data store, uses unmanaged memory back end
+    /// </summary>
+    public class CameraRGBFrameData : IDisposable
+    {
+        private IntPtr dataPointer;
+
+        /// <summary>
+        /// Data pointer for frame data
+        /// </summary>
+        public IntPtr DataPointer
+        {
+            get
+            {
+                if (this.dataPointer == IntPtr.Zero)
+                    throw new ObjectDisposedException("CameraRGBFrameData");
+
+                return this.dataPointer;
+            }
+        }
+
+        /// <summary>
+        /// Constructor, allocates memory to hold frame data
+        /// </summary>
+        public CameraRGBFrameData()
+        {
+            this.dataPointer = Marshal.AllocHGlobal(Consts.DepthWidth * Consts.DepthHeight * Marshal.SizeOf(typeof(CameraSpacePoint)));
+        }
+
+        /// <summary>
+        /// Dispose memory for frame data
+        /// </summary>
+        public void Dispose()
+        {
+            if (this.dataPointer != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(this.dataPointer);
+                this.dataPointer = IntPtr.Zero;
+            }
+        }
+    }   
+        
+}
---
+++ tests/KGP.Tests/Frames/CameraRGBFrameDataTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KGP.Frames;
+
+namespace KGP.Tests
+{
+    [TestClass]
+    public class CameraRGBFrameDataTests
+    {
+        [TestMethod]
+        public void TestConstrutor()
+        {
+            CameraRGBFrameData data = new CameraRGBFrameData();
+            bool pass = data.DataPointer != IntPtr.Zero;
+            data.Dispose();
+            Assert.AreEqual(true, pass);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void TestDispose()
+        {
+            CameraRGBFrameData data = new CameraRGBFrameData();
+            data.Dispose();
+            Assert.AreEqual(data.DataPointer, IntPtr.Zero);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void TestDisposeAccess()
+        {
+            CameraRGBFrameData data = new CameraRGBFrameData();
+            data.Dispose();
+
+            //Should throw exception
+            var pointer = data.DataPointer;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void TestMultipleDispose()
+        {
+            CameraRGBFrameData data = new CameraRGBFrameData();
+            data.Dispose();
+            //Second call to dispose should do nothing
+            data.Dispose();
+            
+            Assert.AreEqual(data.DataPointer, IntPtr.Zero);
+        }
+    }
+}
