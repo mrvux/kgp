0208bcb1a89c7311aa00026a0a0a77fc1829883d
Julien Vulliet <julien@mrvux.com>
2015-03-19 15:04:24 +0100

    Add standard tests for infrared

---
+++ tests/KDP.Direct3D11.Tests/Textures/DynamicInfraredTextureTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KGP.Direct3D11.Textures;
+using KGP.Frames;
+
+namespace KDP.Direct3D11.Tests.Textures
+{
+    [TestClass]
+    public class DynamicInfraredTextureTests : IDisposable
+    {
+        private SharpDX.Direct3D11.Device device;
+
+
+        public DynamicInfraredTextureTests()
+        {
+            device = new SharpDX.Direct3D11.Device(SharpDX.Direct3D.DriverType.Reference);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            using (DynamicInfraredTexture texture = new DynamicInfraredTexture(device))
+            {
+                Assert.AreNotEqual(texture.ShaderView.NativePointer, IntPtr.Zero);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullDevice()
+        {
+            using (DynamicInfraredTexture texture = new DynamicInfraredTexture(null))
+            {
+            }
+        }
+
+        [TestMethod]
+        public void TestCopy()
+        {
+            using (InfraredFrameData frame = new InfraredFrameData())
+            {
+                using (DynamicInfraredTexture texture = new DynamicInfraredTexture(device))
+                {
+                    texture.Copy(device.ImmediateContext, frame);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            device.Dispose();
+        }
+    }
+}
