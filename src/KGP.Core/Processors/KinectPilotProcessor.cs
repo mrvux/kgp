0b70587bef3515cbaf12a40dcec7881d29e96674
Julien Vulliet <julien@mrvux.com>
2015-03-24 17:19:38 +0100

    Add Z steering for pilot

--- src/KGP.Core/Processors/KinectPilotProcessor.cs
+++ src/KGP.Core/Processors/KinectPilotProcessor.cs
@@ -17,15 +17,21 @@ namespace KGP.Processors
         private readonly ICurve pushCurve;
         private readonly ICurve elevationCurve;
 
-        private float steering;
+        private float steeringY;
+        private float steeringZ;
         private float push;
         private float elevation;
 
-        public float Steering
+        public float SteeringY
         {
-            get { return this.steering; }
+            get { return this.steeringY; }
         }
 
+        public float SterringZ
+        {
+            get { return this.steeringZ; }
+        }
+        
         public float Push
         {
             get { return this.push; }
@@ -36,6 +42,12 @@ namespace KGP.Processors
             get { return this.elevation; }
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="steeringCurve">Steering curve</param>
+        /// <param name="pushCurve">Push amount curve</param>
+        /// <param name="elevationCurve">Elevation curve</param>
         public KinectPilotProcessor(ICurve steeringCurve, ICurve pushCurve, ICurve elevationCurve)
         {
             if (steeringCurve == null)
@@ -50,6 +62,9 @@ namespace KGP.Processors
             this.elevationCurve = elevationCurve;
         }
 
+        /// <summary>
+        /// Simple default with a plain linear curve
+        /// </summary>
         public static KinectPilotProcessor Default
         {
             get
@@ -59,6 +74,10 @@ namespace KGP.Processors
             }
         }
 
+        /// <summary>
+        /// Process joint table
+        /// </summary>
+        /// <param name="jointTable"></param>
         public void Process(KinectJointTable jointTable)
         {
             var leftHand = jointTable.Joints[JointType.HandLeft];
@@ -67,12 +86,14 @@ namespace KGP.Processors
             var hipRight = jointTable.Joints[JointType.HipRight];
             var shoulderLeft = jointTable.Joints[JointType.ShoulderLeft];
             var shoulderRight = jointTable.Joints[JointType.ShoulderRight];
+            var elbowLeft = jointTable.Joints[JointType.ElbowLeft];
+            var elbowRight = jointTable.Joints[JointType.ElbowRight];
 
             //First compute elevation, we consider min y at hip and max y at shoulder
             float leftE = ((leftHand.Y - hipLeft.Y) / (shoulderLeft.Y - hipLeft.Y)) * 2.0f - 1.0f;
             float rightE = ((rightHand.Y - hipRight.Y) / (shoulderRight.Y - hipRight.Y)) * 2.0f - 1.0f;
 
-            this.elevation = this.elevationCurve.Apply((leftE + rightE) * 0.5f);
+            this.elevation = this.elevationCurve.Apply(MathUtil.Clamp((leftE + rightE) * 0.5f, -1.0f,1.0f));
 
             float handDirX = leftHand.X - rightHand.X;
             float handDirY = leftHand.Y - rightHand.Y;
@@ -91,9 +112,32 @@ namespace KGP.Processors
                 ang = (ang - pi) / (-halfpi);
             }
 
-            this.steering = this.steeringCurve.Apply(ang);
+            this.steeringY = this.steeringCurve.Apply(MathUtil.Clamp(ang, -1.0f, 1.0f));
 
-            //Last computes push ammount
+            //Build z steering as per star wars game, hand angle in xz plane
+            float handDirZ = leftHand.Z - rightHand.Z;
+            float zang = Convert.ToSingle(Math.Atan2(handDirZ, handDirX));
+
+            if (zang < 0.0f)
+            {
+                zang = (1.0f - (zang + halfpi) / (-halfpi)) * -1.0f;
+            }
+            else
+            {
+                zang = (zang - pi) / (-halfpi);
+            }
+
+            this.steeringZ = zang;
+
+
+            //Last computes push amount, here we say min push is Z next to shoulder and max push is shoulder + arm length
+            float leftArmLength = Vector3.Distance(shoulderLeft, elbowLeft) + Vector3.Distance(elbowLeft, leftHand);
+            float rightArmLength = Vector3.Distance(shoulderRight, elbowRight) + Vector3.Distance(elbowRight, rightHand);
+
+            float leftP = ((shoulderLeft.Z - leftHand.Z) / (leftArmLength)) * 2.0f - 1.0f;
+            float rightP = ((shoulderRight.Z -rightHand.Z) / (rightArmLength)) * 2.0f - 1.0f;
+
+            this.push = this.pushCurve.Apply(MathUtil.Clamp((leftP + rightP) * 0.5f, -1.0f, 1.0f));
         }
     }
 }
