ab11891d4417e6cf7942a5f295375d964777e945
Julien Vulliet <julien@mrvux.com>
2015-03-25 13:41:51 +0100

    Add DynamicColorRGBATextureProcessor and Test suite for it

---
+++ src/KGP.Direct3D11/Processors/DynamicColorRGBATextureProcessor.cs
@@ -0,0 +1,84 @@
+﻿using KGP.Direct3D11.Textures;
+using KGP.Frames;
+using KGP.Providers;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGP.Direct3D11.Processors
+{
+    /// <summary>
+    /// Simple wrapper to provide a color texture upload workflow
+    /// </summary>
+    public class DynamicColorRGBATextureProcessor : IDisposable
+    {
+        private readonly IColorRGBAFrameProvider colorFrameProvider;
+        private ColorRGBAFrameData currentFrameData;
+        private DynamicColorRGBATexture colorTexture;
+        private bool upload;
+
+        /// <summary>
+        /// Color Texture
+        /// </summary>
+        public DynamicColorRGBATexture Texture
+        {
+            get { return this.colorTexture; }
+        }
+
+        /// <summary>
+        /// Tells if our frame needs to be uploaded to gpu
+        /// </summary>
+        public bool NeedUpdate
+        {
+            get { return this.upload; }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="colorFrameProvider">Color frame provider</param>
+        /// <param name="device">Direct3D11 device</param>
+        public DynamicColorRGBATextureProcessor(IColorRGBAFrameProvider colorFrameProvider, Device device)
+        {
+            if (colorFrameProvider == null)
+                throw new ArgumentNullException("colorFrameProvider");
+            if (device == null)
+                throw new ArgumentNullException("device");
+
+            this.colorFrameProvider = colorFrameProvider;
+            this.colorFrameProvider.FrameReceived += FrameReceived;
+            this.colorTexture = new DynamicColorRGBATexture(device);
+        }
+
+        private void FrameReceived(object sender, ColorRGBAFrameDataEventArgs e)
+        {
+            this.currentFrameData = e.FrameData;
+            this.upload = true;
+        }
+
+        /// <summary>
+        /// Copy last frame data to gpu, if applicable
+        /// </summary>
+        /// <param name="context">Device context</param>
+        public void Update(DeviceContext context)
+        {
+            if (upload)
+            {
+                this.colorTexture.Copy(context, this.currentFrameData);
+                upload = false;
+            }
+        }
+        
+        /// <summary>
+        /// Disposes resources
+        /// </summary>
+        public void Dispose()
+        {
+            this.colorTexture.Dispose();
+            this.colorFrameProvider.FrameReceived -= FrameReceived;       
+        }
+    }
+}
---
+++ tests/KDP.Direct3D11.Tests/Processors/DynamicColorRGBATextureProcessorTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KGP.Direct3D11.Textures;
+using KGP.Frames;
+using KGP.Direct3D11.Processors;
+using KGP.Providers;
+
+namespace KDP.Direct3D11.Tests.Textures
+{
+    public class DummyColorRGBAProvider : IColorRGBAFrameProvider , IDisposable
+    {
+        private ColorRGBAFrameData frameData;
+
+        public ColorRGBAFrameData FrameData
+        {
+            get { return this.frameData; }
+        }
+
+        public DummyColorRGBAProvider()
+        {
+            this.frameData = new ColorRGBAFrameData();
+        }
+
+        public void PushFrame()
+        {
+            if (this.FrameReceived!= null)
+            {
+                this.FrameReceived(this, new ColorRGBAFrameDataEventArgs(this.frameData));
+            }
+        }
+
+        public void Dispose()
+        {
+            this.frameData.Dispose();
+        }
+
+        public event EventHandler<ColorRGBAFrameDataEventArgs> FrameReceived;
+    }
+
+
+    [TestClass]
+    public class DynamicColorRGBATextureProcessorTests : IDisposable
+    {
+        private SharpDX.Direct3D11.Device device;
+
+
+        public DynamicColorRGBATextureProcessorTests()
+        {
+            device = new SharpDX.Direct3D11.Device(SharpDX.Direct3D.DriverType.Reference);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            using (DummyColorRGBAProvider provider = new DummyColorRGBAProvider())
+            {
+                using (DynamicColorRGBATextureProcessor textureProcessor = new DynamicColorRGBATextureProcessor(provider, device))
+                {
+                    Assert.IsFalse(textureProcessor.Texture.ShaderView.NativePointer == IntPtr.Zero);
+                    Assert.IsFalse(textureProcessor.NeedUpdate);
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullDevice()
+        {
+            using (DummyColorRGBAProvider provider = new DummyColorRGBAProvider())
+            {
+                using (DynamicColorRGBATextureProcessor textureProcessor = new DynamicColorRGBATextureProcessor(provider, null))
+                {
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullProvider()
+        {
+            using (DynamicColorRGBATextureProcessor textureProcessor = new DynamicColorRGBATextureProcessor(null, device))
+            {
+            }
+        }
+
+        [TestMethod]
+        public void TestReceive()
+        {
+            using (DummyColorRGBAProvider provider = new DummyColorRGBAProvider())
+            {
+                using (DynamicColorRGBATextureProcessor textureProcessor = new DynamicColorRGBATextureProcessor(provider, device))
+                {
+                    provider.PushFrame();
+                    Assert.IsTrue(textureProcessor.NeedUpdate);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestUpload()
+        {
+            using (DummyColorRGBAProvider provider = new DummyColorRGBAProvider())
+            {
+                using (DynamicColorRGBATextureProcessor textureProcessor = new DynamicColorRGBATextureProcessor(provider, device))
+                {
+                    provider.PushFrame();
+                    Assert.IsTrue(textureProcessor.NeedUpdate);
+                    textureProcessor.Update(device.ImmediateContext);
+                    Assert.IsFalse(textureProcessor.NeedUpdate);
+                }
+            }
+        }
+
+
+        public void Dispose()
+        {
+            device.Dispose();
+        }
+    }
+}
