a2b018e8ab206cbfae6a7288f345404fe23414d7
mrvux <julien@mrvux.com>
2015-03-27 14:25:08 +0100

    Comment and fix on namespace

--- src/KGP.Core/Curves/ThresholdLinearCurve.cs
+++ src/KGP.Core/Curves/ThresholdLinearCurve.cs
@@ -4,23 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KGP.Curves
+namespace KGP
 {
+    /// <summary>
+    /// Linear threshold curve, clamp while absolute value is within threshold
+    /// </summary>
     public class ThresholdLinearCurve : ICurve
     {
         private float threshold;
 
+        /// <summary>
+        /// Current threshold
+        /// </summary>
         public float Threshold
         {
             get { return this.threshold; }
             set { this.threshold = value; }
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="threshold">Initial threshold value</param>
         public ThresholdLinearCurve(float threshold)
         {
             this.threshold = threshold;
         }
 
+        /// <summary>
+        ///Apply threshold curve
+        /// </summary>
+        /// <param name="value">Intial value</param>
+        /// <returns>Value with curve applied</returns>
         public float Apply(float value)
         {
             float absval = Math.Abs(value);
